0cf02a6386ff24d5a365b3481be57e950b82f85b
vishalmorey1998 <vishalmorey2000@gmail.com>
2024-11-12 15:05:23 +0530

    Update Demo.java

--- javaDemo/src/com/Demo.java
+++ javaDemo/src/com/Demo.java
@@ -5,6 +5,10 @@ public class Demo {
 	{
 		System.out.println("this is add method");
 	}
+	public void sub()
+	{
+		System.out.println("this is sub method");
+	}
 
 	public static void main(String[] args) {
 		System.out.println("This is a Github project");
