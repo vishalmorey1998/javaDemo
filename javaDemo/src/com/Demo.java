fd5bf0c2edb94a4c43eb8e56b34ea2c9b2f2fc48
HP <vishalmorey1998@gmail.com>
2024-11-12 15:04:04 +0530

    by pooja

--- javaDemo/src/com/Demo.java
+++ javaDemo/src/com/Demo.java
@@ -1,6 +1,10 @@
 package com;
 
 public class Demo {
+	public void add()
+	{
+		System.out.println("this is add method");
+	}
 
 	public static void main(String[] args) {
 		System.out.println("This is a Github project");
